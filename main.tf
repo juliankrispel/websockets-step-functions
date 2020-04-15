5381eb96e93c1dfbc326d5cef15c82e2380f30ff
Julian Krispel-Samsel <jkrispel-samsel@futurefinance.com>
2020-04-15 21:57:24 +0100

    update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,6 @@ resource "aws_iam_role" "iam_for_sfn" {
   assume_role_policy = file("policies/iam_for_sfn.json")
 }
 
-
 resource "aws_lambda_function" "lambda" {
   for_each = fileset(path.module, "lambdas/*.js")
 
@@ -31,4 +30,12 @@ resource "aws_sfn_state_machine" "state_machine" {
   role_arn = aws_iam_role.iam_for_sfn.arn
 
   definition = file("./states.json")
+}
+
+output "lambdas" {
+  value = {
+    for lambda in aws_lambda_function.lambda:
+    lambda.function_name
+  }
+
 }
