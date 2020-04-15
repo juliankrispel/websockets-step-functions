ad6e91c0c61d14c20f9859419c45f7b4aa80e216
Julian Krispel-Samsel <jkrispel-samsel@futurefinance.com>
2020-04-15 21:40:30 +0100

    extract json and add state machine

--- main.tf
+++ main.tf
@@ -5,32 +5,30 @@ provider "aws" {
 
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+  assume_role_policy = file("policies/iam_for_lambda.json")
 }
 
-resource "aws_lambda_function" "test_lambda" {
+resource "aws_iam_role" "iam_for_sfn" {
+  name = "iam_for_sfn"
+  assume_role_policy = file("policies/iam_for_sfn.json")
+}
+
+
+resource "aws_lambda_function" "lambda" {
   for_each = fileset(path.module, "lambdas/*.js")
 
   filename      = each.value
-  function_name = each.value
+  function_name = replace(basename(each.value), ".js", "")
   role          = "${aws_iam_role.iam_for_lambda.arn}"
   handler       = "exports.handler"
 
   source_code_hash = filebase64(each.value)
   runtime = "nodejs12.x"
+}
+
+resource "aws_sfn_state_machine" "state_machine" {
+  name     = "websocket-state-machine"
+  role_arn = aws_iam_role.iam_for_sfn.arn
+
+  definition = file("./states.json")
 }
