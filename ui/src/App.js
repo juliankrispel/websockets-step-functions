7e1db4055d0b61d9df4e16b833d63d518672d1bb
Julian Krispel-Samsel <julian@jkrsp.com>
2021-03-13 10:19:06 +0000

    update

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useCallback, useEffect } from 'react';
 import './App.css';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 
 function App() {
+  const [socketUrl, setSocketUrl] = useState(null); //Public API that will echo messages sent to it back to the client
+  const [messageHistory, setMessageHistory] = useState([]);
+  const [sendMessage, lastMessage, readyState, getWebSocket] = useWebSocket(socketUrl);
+ 
+  const handleClickChangeSocketUrl = useCallback(() => setSocketUrl('wss://mhyvm16uxb.execute-api.eu-west-2.amazonaws.com/v1'), []);
+  const handleClickSendMessage = useCallback(() => sendMessage('Hello'), []);
+ 
+  useEffect(() => {
+    if (lastMessage !== null) {
+    
+      //getWebSocket returns the WebSocket wrapped in a Proxy. This is to restrict actions like mutating a shared websocket, overwriting handlers, etc
+      const currentWebsocketUrl = getWebSocket().url;
+      console.log('received a message from ', currentWebsocketUrl);
+      
+      setMessageHistory(prev => prev.concat(lastMessage));
+    }
+  }, [lastMessage]);
+ 
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: 'Connecting',
+    [ReadyState.OPEN]: 'Open',
+    [ReadyState.CLOSING]: 'Closing',
+    [ReadyState.CLOSED]: 'Closed',
+    '-1': 'Not Connected'
+  }[readyState];
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleClickChangeSocketUrl}>Start</button>
+      <button onClick={handleClickSendMessage} disabled={readyState !== ReadyState.OPEN}>Click Me to send 'Hello'</button>
+      <span>The WebSocket is currently {connectionStatus}</span>
+      <ul>
+        {messageHistory.map((message, idx) => <span key={idx}>{message.data}</span>)}
+      </ul>
     </div>
   );
 }
